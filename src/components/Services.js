2cdeb3d5c74ae30579927d713441d5adaabb23bf
Martez Conner <conner.martez@gmail.com>
2019-10-22 13:10:09 -0700

    style service component

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,6 +2,7 @@ import React from "react"
 
 const Services = () => (
   <div className="contentContainer">
+    <div className="serviceTitle">Our Services</div>
     <div className='services'>
       <h3 className='service-1'>ui design</h3>
       <h3 className='service-2'>mobile app development</h3>
