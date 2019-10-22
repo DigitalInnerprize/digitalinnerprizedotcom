763c12196f34877e77cf269cfadd5492233a08ff
Martez Conner <conner.martez@gmail.com>
2019-10-22 09:30:06 -0700

    attempt netlify build error fix with git ignoring case files

---
+++ src/components/Layout.js
@@ -0,0 +1,43 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./Header"
+import "../styles/styles.scss"
+
+const Layout = ({ children, page = '' }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  console.log(page)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+        <main>{children}</main>
+        <footer style={{ backgroundColor: page !== 'home' ? 'white' : '#f9f9f9'}}>
+          © {new Date().getFullYear()}, <span>Built by <p>Digital InnerPrize L.L.C</p></span>
+        </footer>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
