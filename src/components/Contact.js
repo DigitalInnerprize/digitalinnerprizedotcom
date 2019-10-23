8576bdc01af1f9a2a88eb5395565d71e6d5d2be5
Martez Conner <conner.martez@gmail.com>
2019-10-23 10:36:51 -0700

    fix image display none bug

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,21 +20,23 @@ const Contact = () => {
   }
 
   return (
-    <div className='contactContainer'>
-      <div className="contactImage">
-        <Img
-          sizes={contactImage.childImageSharp.sizes}
-        />
-      </div>
-        <div id="contactText">
-          <h3 className='contactTitle'>Let's Talk Business!</h3>
-          <p>A digital agency for brands that focus on creating great work and that gets phenomenal results along with a unique digital presence.</p>
-          <button 
-            className='contactBtn'
-            onClick={onClick}>
-              Contact Us
-          </button>
+    <div className="contactPageContainer">
+      <div className='contactContainer'>
+        <div className="contactImage">
+          <Img
+            sizes={contactImage.childImageSharp.sizes}
+          />
         </div>
+          <div id="contactText">
+            <h3 className='contactTitle'>Let's Talk Business!</h3>
+            <p>A digital agency for brands that focus on creating great work and that gets phenomenal results along with a unique digital presence.</p>
+            <button 
+              className='contactBtn'
+              onClick={onClick}>
+                Contact Us
+            </button>
+          </div>
+      </div>
     </div>
   )
 }
