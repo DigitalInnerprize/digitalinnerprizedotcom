6c74bf0aba3d9b769ad347e3ad5d4c0c1521e741
Martez Conner <conner.martez@gmail.com>
2019-10-27 18:31:10 -0700

    style aboutPage image & component

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -8,9 +8,6 @@ const AboutUsPage = () => (
   <Layout page='aboutUs'>
     <SEO title="About Us" />
     <div id="aboutUsContainer">
-      <div className="circle-1"></div>
-      <div className="circle-2"></div>
-      <div className="circle-3"></div>
         <h3>At Digital InnerPrize we focus on bringing a digital presence to any platform for any business. From working on enterprise level consumer facing applications to custom websites for individuals looking to grow their brand or small business.</h3>
         <h3> We offer a wide variety of services from web design to developing mobile applications that fits our clients needs. Our aim is to have our clients stand out from the rest of the crowd and make their presence known.</h3>
     </div>
